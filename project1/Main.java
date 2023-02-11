98c225cf13553cb2f53c695622010c3e6e581d4a
Jayparkrighthere <zoeytwelve@gmail.com>
2023-02-11 16:45:19 -0700

    finished part1

--- project1/Main.java
+++ project1/Main.java
@@ -1,7 +1,14 @@
 package project1;
 
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
-
+        Optional<ShareInfo> highShare = PickShareFunctional.findHighPriced(Shares.symbols.stream());
+        if (highShare.isPresent()) {
+            System.out.println("High priced uder $500 is " + highShare.get());
+        } else {
+            System.out.println("No High priced uder $500 exist");
+        }
     }
 }
