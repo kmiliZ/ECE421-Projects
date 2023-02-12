23ce235de4c63b8a467058d34a6e7479d1b81bea
Jayparkrighthere <zoeytwelve@gmail.com>
2023-02-11 19:00:07 -0700

    api key

--- project1/APIFinance.java
+++ project1/APIFinance.java
@@ -9,7 +9,7 @@ import java.net.URLConnection;
 
 public class APIFinance {
     private static final String BASE_URL = "https://www.alphavantage.co/query?";
-    private final static String apiKey = "YOUR_API_KEY_HERE";
+    private final static String apiKey = "8WBJ9AS0QW5XAZYD";
 
     public static BigDecimal getPrice(final String symbol) {
         BigDecimal price = new BigDecimal(0);
