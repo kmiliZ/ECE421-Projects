bb5709ff41486f755add5784b0b56d858ae72c75
Carl <tianyuan@ualberta.ca>
2023-02-11 23:37:41 -0700

    project created

---
+++ project1/project1/src/project1/Shares.java
@@ -0,0 +1,8 @@
+package project1;
+
+import java.util.*;
+
+public class Shares {
+    public static final List<String> symbols = Arrays.asList("IBM", "AAPL", "AMZN", "CSCO", "SNE", "GOOG", "MSFT",
+            "ORCL", "FB", "VRS N");
+}
