c92bd29a855b332c8ee238b63c6ce2ac5b86225e
Jayparkrighthere <zoeytwelve@gmail.com>
2023-02-13 20:32:12 -0700

    refactor APIFinance

--- project1/APIFinance.java
+++ project1/APIFinance.java
@@ -11,46 +11,44 @@ public class APIFinance {
     private static final String BASE_URL = "https://www.alphavantage.co/query?";
     private final static String apiKey = "O0IORAI4LIZIK61W";
 
-    private static int limitCount = 0;
-    private static long startTime = 0;
+    private static int consecutiveSleelp = 0;
     private static final long timeLimit = 60000;
-    private static boolean is_resend = true;
 
     public static BigDecimal getPrice(final String symbol) {
         // sleepForLimit();
         BigDecimal price = new BigDecimal(0);
-        is_resend = true;
-        while (is_resend) {
-            is_resend = false;
-            System.out.println("Sending out new request");
-            try {
-                URL url = new URL(BASE_URL +
-                        "function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + apiKey);
-                URLConnection connection = url.openConnection();
-                InputStreamReader inputStream = new InputStreamReader(connection.getInputStream(), "UTF-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStream);
-                String line;
-                while ((line = bufferedReader.readLine()) != null) {
 
-                    if (line.contains("price")) {
-                        price = new BigDecimal(line.split("\"")[3].trim());
+        try {
+            URL url = new URL(BASE_URL +
+                    "function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + apiKey);
+            URLConnection connection = url.openConnection();
+            InputStreamReader inputStream = new InputStreamReader(connection.getInputStream(), "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStream);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
 
-                    } else if (line
-                            .contains("Our standard API call frequency is 5 calls per minute and 500 calls per day.")) {
+                if (line.contains("price")) {
+                    price = new BigDecimal(line.split("\"")[3].trim());
+                    consecutiveSleelp = 0;
+                } else if (line
+                        .contains("Our standard API call frequency is 5 calls per minute and 500 calls per day.")) {
+                    if (consecutiveSleelp == 0) {
                         try {
                             Thread.sleep(timeLimit);
-                            is_resend = true;
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
-                        is_resend = true;
-
+                        consecutiveSleelp += 1;
+                        return getPrice(symbol);
+                    } else {
+                        System.out.println("Exceeded API call limitation for today!!!");
                     }
+
                 }
-                bufferedReader.close();
-            } catch (IOException e) {
-                System.out.println("failure sending request");
             }
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.out.println("failure sending request");
         }
 
         return price;
--- project1/Main.java
+++ project1/Main.java
@@ -6,12 +6,13 @@ public class Main {
     public static void main(String[] args) {
         long startTime = System.currentTimeMillis();
 
-        Optional<ShareInfo> highShare = PickShareFunctional.findHighPriced(Shares.symbols.stream());
+        Optional<ShareInfo> highShare = PickShareFunctional.findHighPriced(Shares.symbols.parallelStream());
 
         long endTime1 = System.currentTimeMillis();
 
-        Optional<ShareInfo> highShareParallel = PickShareFunctional.findHighPriced(Shares.symbols.parallelStream());
-        long endTime2 = System.currentTimeMillis();
+        // Optional<ShareInfo> highShareParallel =
+        // PickShareFunctional.findHighPriced(Shares.symbols.parallelStream());
+        // long endTime2 = System.currentTimeMillis();
 
         long duration1 = (endTime1 - startTime);
         if (highShare.isPresent()) {
@@ -21,15 +22,16 @@ public class Main {
         }
         System.out
                 .println("Time execution of findHighPriced(Shares.symbols.stream()) = " + duration1 + " milliseconds");
-        long duration2 = (endTime2 - endTime1);
+        // long duration2 = (endTime2 - endTime1);
 
-        if (highShareParallel.isPresent()) {
-            System.out.println("High priced uder $500 is " + highShareParallel.get());
-        } else {
-            System.out.println("No High priced uder $500 exist");
-        }
-        System.out.println(
-                "Time execution of findHighPriced(Shares.symbols.parallelStream()) = " + duration2 + " milliseconds");
+        // if (highShareParallel.isPresent()) {
+        // System.out.println("High priced uder $500 is " + highShareParallel.get());
+        // } else {
+        // System.out.println("No High priced uder $500 exist");
+        // }
+        // System.out.println(
+        // "Time execution of findHighPriced(Shares.symbols.parallelStream()) = " +
+        // duration2 + " milliseconds");
 
     }
 }
