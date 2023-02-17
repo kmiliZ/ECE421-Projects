f08322feb1f03ef964b6d21e09a208be2d6d9c26
Jayparkrighthere <zoeytwelve@gmail.com>
2023-02-16 22:51:50 -0700

    delete redundent dir

--- project1/Shares.java
+++
@@ -1,8 +0,0 @@
-package project1;
-
-import java.util.*;
-
-public class Shares {
-    public static final List<String> symbols = Arrays.asList("IBM", "AAPL", "AMZN", "CSCO", "SNE", "GOOG", "MSFT",
-            "ORCL", "FB", "VRS N");
-}
