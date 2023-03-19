0dbf79c46b75c8427f7a77c61d2cec02d86d1dd3
JakobLau9 <JakobLau9@gmail.com>
2023-03-18 21:57:40 -0600

    Fixed insert and deletion, added printing strucutre

--- project2/src/avl.rs
+++ project2/src/avl.rs
@@ -3,14 +3,15 @@ use std::cell::RefCell;
 
 #[derive(Debug)]
 struct Node<T> {
-    data: T,
+    data: Option<T>,
     height: i32,
     left: Option<Rc<RefCell<Node<T>>>>,
     right: Option<Rc<RefCell<Node<T>>>>,
 }
 
+
 impl<T> Node<T> {
-    fn new(data: T) -> Rc<RefCell<Self>> {
+    fn new(data: Option<T>) -> Rc<RefCell<Self>> {
         Rc::new(RefCell::new(Node {
             data,
             height: 1,
@@ -19,6 +20,7 @@ impl<T> Node<T> {
         }))
     }
 }
+
 
 fn height<T>(node: &Option<Rc<RefCell<Node<T>>>>) -> i32 {
     node.as_ref().map_or(0, |n| n.borrow().height)
@@ -55,25 +57,43 @@ fn rotate_right<T>(node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
     update_height(&left.borrow().right);
     left
 }
+// This rotates the given node left and then right
+fn rotate_left_right<T>(node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
+    let left = node.borrow().left.as_ref().unwrap().clone();
+    let new_left = rotate_left(left.clone());
+    node.borrow_mut().left = Some(new_left.clone());
+    rotate_right(node)
+}
+
+// This rotates the given node right and then left
+fn rotate_right_left<T>(node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
+    let right = node.borrow().right.as_ref().unwrap().clone();
+    let new_right = rotate_right(right.clone());
+    node.borrow_mut().right = Some(new_right.clone());
+    rotate_left(node)
+}
 
 // https://www.youtube.com/watch?v=vRwi_UcZGjU for explanation on rotation balance logic
-fn rebalance<T>(node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
+fn rebalance<T: Clone>(node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
     let balance = balance_factor(&Some(node.clone()));
     if balance > 1 {
         // When tree is right-heavy, perform left rotation then right rotation, otherwise just do a right rotation
-        
         let left_balance = balance_factor(&node.borrow().left);
         if left_balance < 0 {
-            node.borrow_mut().left = Some(rotate_left(node.borrow().left.as_ref().unwrap().clone()));
+            rotate_left_right(node)
+        } else {
+            rotate_right(node)
         }
-        rotate_right(node)
+
     } else if balance < -1 {
         // When tree is left-heavy, perform right rotation then left rotation
         let right_balance = balance_factor(&node.borrow().right);
         if right_balance > 0 {
-            node.borrow_mut().right = Some(rotate_right(node.borrow().right.as_ref().unwrap().clone()));
+            rotate_right_left(node)
+        } else {
+            rotate_left(node)
         }
-        rotate_left(node)
+        
     } else {
         node
     }
@@ -90,7 +110,7 @@ impl<T: std::cmp::Ord + Clone + Default + std::fmt::Debug> AVLTree<T> {
     }
 
     pub fn insert(&mut self, data: T) {
-        let node = Node::new(data);
+        let node = Node::new(Some(data));
         self.root = match self.root.take() {
             None => Some(node.clone()),
             Some(root) => Some(Self::insert_recursive(root, node))
@@ -139,7 +159,7 @@ impl<T: std::cmp::Ord + Clone + Default + std::fmt::Debug> AVLTree<T> {
                 }
              );  
         // Travese right if the data is larger than the current root
-        } else {
+        } else if node_data > root_data {
             root.replace_with(|old| 
                 match old {
                     Node{data, height, left: Some(y), right: None} => {
@@ -177,39 +197,66 @@ impl<T: std::cmp::Ord + Clone + Default + std::fmt::Debug> AVLTree<T> {
                 }
             );
         }
+        // If the value inserted already exists in the tree then nothing happens
         update_height(&Some(root.clone()));
         rebalance(root)
     }
 
     pub fn delete(&mut self, data: T) {
-        let node = Node::new(data);
-        self.root = match self.root.take() {
-            None => Some(node.clone()),
-            Some(root) => Some(Self::delete_recursive(root, node))
-        };
+        let node = Node::new(Some(data));
+        if let Some(root) = self.root.take() {
+            let new_root = Self::delete_recursive(Rc::clone(&root), node);
+            // Deletes the root if the only value is None
+            self.root = if new_root.borrow().data.is_some() {
+                Some(new_root)
+            } else {
+                None
+            };
+        }
     }
 
     fn delete_recursive(root: Rc<RefCell<Node<T>>>, node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
         let root_data = root.borrow().data.clone();
         let node_data = node.borrow().data.clone();
         // Check which side the deleted node is going to be on
+        // Data is smaller than current node, so travers left
         if node_data < root_data {
             root.replace_with(|old| 
                 match old {
                     Node{data, height, left: Some(x), right: Some(y)} => {
-                        Node {
-                            data: data.clone(),
-                            height: height.clone(),
-                            left: Some(Self::delete_recursive(x.clone(), node.clone())),
-                            right: Some(Rc::clone(y)),
+                        let temp = Self::delete_recursive(x.clone(), node.clone());
+                        if temp.borrow().data.clone().is_none(){
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: None,
+                                right: Some(Rc::clone(y)),
+                            } 
+                        } else {
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: Some(temp),
+                                right: Some(Rc::clone(y)),
+                            } 
                         }
                     },
                     Node{data, height, left: Some(x), right: None} => {
-                        Node {
-                            data: data.clone(),
-                            height: height.clone(),
-                            left: Some(Self::delete_recursive(x.clone(), node.clone())),
-                            right: None,
+                        let temp = Self::delete_recursive(x.clone(), node.clone());
+                        if temp.borrow().data.clone().is_none(){
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: None,
+                                right: None,
+                            } 
+                        } else {
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: Some(temp),
+                                right: None,
+                            } 
                         }
                     },
                     Node{data, height, left: None, right: None} => {
@@ -230,26 +277,46 @@ impl<T: std::cmp::Ord + Clone + Default + std::fmt::Debug> AVLTree<T> {
                     },
                 }
              ); 
-             update_height(&Some(node.clone()));
-             rebalance(node.clone())
-             
+             update_height(&Some(root.clone()));
+             rebalance(root.clone())
+        // Data is larger than current node so traverse right
         } else if node_data > root_data {
             root.replace_with(|old| 
                 match old {
                     Node{data, height, left: Some(x), right: Some(y)} => {
-                        Node {
-                            data: data.clone(),
-                            height: height.clone(),
-                            left: Some(Rc::clone(x)),
-                            right: Some(Self::delete_recursive(y.clone(), node.clone())),
+                        let temp = Self::delete_recursive(y.clone(), node.clone());
+                        if temp.borrow().data.clone().is_none(){
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: Some(Rc::clone(x)),
+                                right: None,
+                            } 
+                        } else {
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: Some(Rc::clone(x)),
+                                right: Some(temp),
+                            } 
                         }
                     },
                     Node{data, height, left: None, right: Some(y)} => {
-                        Node {
-                            data: data.clone(),
-                            height: height.clone(),
-                            left: None,
-                            right: Some(Self::delete_recursive(y.clone(), node.clone())),
+                        let temp = Self::delete_recursive(y.clone(), node.clone());
+                        if temp.borrow().data.clone().is_none(){
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: None,
+                                right: None,
+                            } 
+                        } else {
+                            Node {
+                                data: data.clone(),
+                                height: height.clone(),
+                                left: None,
+                                right: Some(temp),
+                            } 
                         }
                     },
                     Node{data, height, left: None, right: None} => {
@@ -274,100 +341,69 @@ impl<T: std::cmp::Ord + Clone + Default + std::fmt::Debug> AVLTree<T> {
              rebalance(root.clone())
 
         } else {
-            /*
             // Node to be deleted found on the current node
             // Now have to replace the node with a new node
-            if node.borrow().left.is_none() && node.borrow().right.is_none(){
-                // TODO: Case 1: Node has no children
-
-            } else if node.borrow().left.is_none() {
-                // Case 2: Node has no left child
-                let right_child = node.borrow_mut().right.take();
-                node = right_child.unwrap().clone();
-
-            } else if node.borrow().right.is_none() {
-                // Case 3: Node has no right child
-                let left_child = node.borrow_mut().left.take();
-                node = left_child.unwrap().clone();
-
-            } else {
-                // Case 4: Node has both left and right children
-                // Find the smallest child on the right side and replace the deleted node with that
-                let mut min_node = Rc::clone(&node.borrow().right.as_ref().unwrap());
-                let mut left_child = min_node.borrow().left.clone();
-                while min_node.borrow().left.is_some() {
-                    min_node = Rc::clone(&left_child.unwrap());
-                    left_child = min_node.borrow().left.clone();
-                }
-                // Replace the node to be deleted with the minimum node found
-                let new_data = min_node.borrow().data.clone();
-                Self::delete_recursive(node, new_data.clone());
-                node.borrow_mut().data = new_data;
-            }
-            update_height(&Some(node.clone()));
-            rebalance(node.clone())*/
-            root.replace_with(|old| 
+            let _ = &root.replace_with(|old| 
                 match old {
+                    // Case 1: Node has no children
                     // TODO: FIX CASE WHERE THERE IS NO CHILDREN WHEN DELETING
                     Node{data, height, left: None, right: None} => {
+                        println!("NO CHILDREN");
                         Node {
-                            data: T::default(),
+                            data: None,
                             height: 0,
                             left: None,
                             right: None,
                         }
                     },
+                    // Case 2: Node has both left and right children
                     Node{data, height, left: Some(x), right: Some(y)} => {
                         let min = Self::find_min(Rc::clone(y));
-                        let min2 = Self::find_min(Rc::clone(y));
-                        let new_right = Self::delete_recursive(Rc::clone(y), min2);
-                        let x = Node {
-                            data: min.borrow().data.clone(),
-                            height: height.clone(),
-                            left: Some(Rc::clone(x)),
-                            right: Some(new_right),
-                        }; x
+                        if y.borrow().left.clone().is_none() && y.borrow().right.clone().is_none(){
+                            let temp = Node {
+                                data: min.borrow().data.clone(),
+                                height: height.clone(),
+                                left: Some(Rc::clone(x)),
+                                right: None,
+                            }; temp
+                        } else {
+                            let temp = Node {
+                                data: min.borrow().data.clone(),
+                                height: height.clone(),
+                                left: Some(Rc::clone(x)),
+                                right: Some(Rc::clone(y)),
+                            };
+                            Self::delete_recursive(x.clone(), min);
+                            temp
+                        }
                     },
+                    // Case 3: Node has no left child
                     Node{data, height, left: None, right: Some(y)} => {
+                        println!("NO LEFT CHILD");
                         let min = Self::find_min(Rc::clone(y));
-                        let x = Node {
+                        let result = Node {
                             data: min.borrow().data.clone(),
                             height: height.clone(),
                             left: None,
                             right: None,
-                        }; x
+                        }; result
                     },
+                    // Case 4: Node has no right child
                     Node{data, height, left: Some(y), right: None} => {
+                        println!("NO RIGHT CHILD");
                         let min = Self::find_min(Rc::clone(y));
-                        let x = Node {
+                        let result = Node {
                             data: min.borrow().data.clone(),
                             height: height.clone(),
                             left: None,
                             right: None,
-                        }; x
+                        }; result
                     }
                 }
              );
              update_height(&Some(root.clone()));
              rebalance(root.clone())
 
-            /*
-            match (&root.borrow().left, &root.borrow().right) {
-                (None, Some(x)) => Some(Rc::clone(x)),
-                (Some(x), None) => Some(Rc::clone(x)),
-                (Some(x), Some(y)) => {
-                    let min = Self::find_min(Rc::clone(y));
-                    let new_right = Self::delete_recursive(Rc::clone(y), min);
-                    let mut new_root = Node {
-                        data: min.borrow().data.clone(),
-                        height: root.borrow().height.clone(),
-                        left: Some(Rc::clone(x)),
-                        right: Some(new_right),
-                    };
-                    update_height(&Some(new_root));
-                    rebalance(new_root)
-                }
-            }*/
         }
     }
 
@@ -378,6 +414,7 @@ impl<T: std::cmp::Ord + Clone + Default + std::fmt::Debug> AVLTree<T> {
         }
         node
     }
+
 
     // Checks if the root is none, and if it is then the tree is empty
     pub fn is_empty(&self) -> bool {
@@ -425,6 +462,20 @@ impl<T: std::cmp::Ord + Clone + Default + std::fmt::Debug> AVLTree<T> {
             Self::in_order_traversal_recursive(node_borrow.left.as_ref());
             println!("{:?}", node_borrow.data);
             Self::in_order_traversal_recursive(node_borrow.right.as_ref());
+        }
+    }
+
+    // This will be the fucntion that main calls to print the structure of the tree
+    pub fn print_structure(&self) {
+        Self::print_node(&self.root, 0, "Root");
+    }
+
+    // This uses preorder traversal to print out the tree with structure, where the height of the tree is the widge that is used to space out the strucutre. 
+    fn print_node(node: &Option<Rc<RefCell<Node<T>>>>, level: i32, direction: &str) {
+        if let Some(n) = node {
+            println!("{:>width$} {} {:?}", "", direction, n.borrow().data.clone().unwrap(), width = (level * 4) as usize);
+            Self::print_node(&n.borrow().left, level + 1, "Left");
+            Self::print_node(&n.borrow().right, level + 1, "Right");
         }
     }
 
