e11410feb89fb8cfcf636d45a3f1a915450e1728
Jayparkrighthere <zoeytwelve@gmail.com>
2023-04-04 19:17:22 -0600

    structure

---
+++ project3/web/src/pages/connect4.rs
@@ -0,0 +1,3 @@
+// set up listeiner on canvus, grab location to know which grid it is and insert chip
+// import state, game from what Jakob have, and use the function on the game object
+// make animation, display blablabla
